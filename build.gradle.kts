45e736d26137a3accc703ad79de073ba6573a9f2
kmamaguana <kmamaguana@uce.edu.ec>
2025-07-28 17:12:42 -0500

    Despliegue 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,8 @@ val exposed_version = "0.41.1"
 val postgresql_version = "42.6.0"
 val kotlinx_serialization_version = "1.6.0"
 val bcrypt_version = "0.9.0"
+val ktor_client_version = "2.3.4"
+val dotenv_version = "6.4.1"
 
 plugins {
     kotlin("jvm") version "2.1.10"
@@ -23,10 +25,14 @@ repositories {
 }
 
 dependencies {
-    // Ktor core y Netty
+    // Ktor Server Core
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-config-yaml:$ktor_version")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+    implementation("io.ktor:ktor-server-auth:$ktor_version")
+    implementation("io.ktor:ktor-server-auth-jwt:$ktor_version")
 
     // Logging
     implementation("ch.qos.logback:logback-classic:$logback_version")
@@ -40,32 +46,22 @@ dependencies {
     // PostgreSQL Driver
     implementation("org.postgresql:postgresql:$postgresql_version")
 
-    // Seguridad y autenticación
-    implementation("io.ktor:ktor-server-auth:$ktor_version")
-    implementation("io.ktor:ktor-server-auth-jwt:$ktor_version")
+    // Serialización
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinx_serialization_version")
+
+    // Seguridad
     implementation("at.favre.lib:bcrypt:$bcrypt_version")
 
-    // Serialización
-    implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinx_serialization_version")
+    // Ktor Client (para chatbot o peticiones HTTP)
+    implementation("io.ktor:ktor-client-core:$ktor_client_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_client_version")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktor_client_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_client_version")
+
+    // Variables de entorno
+    implementation("io.github.cdimascio:dotenv-kotlin:$dotenv_version")
 
     // Testing
     testImplementation("io.ktor:ktor-server-test-host-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
-
-    //chatbot
-    implementation("io.ktor:ktor-client-core:2.3.3")
-    implementation("io.ktor:ktor-client-cio:2.3.3")
-    implementation("io.ktor:ktor-client-content-negotiation:2.3.3")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.3")
-
-    implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
-
-    // Agrega en tu archivo build.gradle.kts:
-    implementation("io.ktor:ktor-client-core:2.3.4")
-    implementation("io.ktor:ktor-client-cio:2.3.4")
-    implementation("io.ktor:ktor-client-content-negotiation:2.3.4")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 }
